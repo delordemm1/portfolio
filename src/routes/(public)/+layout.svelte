f6c444bf71a64b0d78094fd6cc8f9f05139f7ea6
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2025-07-12 18:43:50 +0100

    layout fix

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Header from "$lib/components/header.svelte";
+  let { children } = $props();
+</script>
+
+<Header />
+<main class="pt-10">{@render children()}</main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-  import Header from '$lib/components/header.svelte';
-	import '../app.css';
-	
-	let { children } = $props();
+  import "../app.css";
+
+  let { children } = $props();
 </script>
-<Header />
-<main class="pt-10">{@render children()}</main>
+
+{@render children()}
